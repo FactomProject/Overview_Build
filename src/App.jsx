b7e6acb6a556fb59a0511c29ae1581cb65abe823
Joshua Brigati <brigati.joshua@gmail.com>
2018-08-29 13:13:16 -0500

    removed useless stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import $ from 'jquery';
 import Table from './components/full-table';
 import io from 'socket.io-client';
-// const io = require('socket.io')();
-require('dotenv').config()
-
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -27,7 +23,7 @@ class App extends React.Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">This is gonna do some sweet stuff</h1>
+          <h1 className="App-title">Information Display</h1>
         </header>
         <div className="row">
           <Table  rowList={this.state.colVals} displayed={this.state.displayed} />
