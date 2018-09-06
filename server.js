2a78b0fe2e5a2479bbaeaf913b281d25d65d0b9a
Joshua Brigati <brigati.joshua@gmail.com>
2018-09-06 15:09:28 -0500

    adding api and ip single call

--- server.js
+++ server.js
@@ -16,18 +16,37 @@ server = app.listen(5001, function(){
 });
 
 let disbish = process.env.IPLIST.split(`','`)
+let apisList = process.env.APILIST.split(`","`)
+let v2List = ["heights","properties"];
+console.log(apisList)
 
 io = socket(server);
 () => {io.emit('ListOfURLs', disbish)}
 
-    io.on('connection', (socket) => {
-    
-        io.emit('ListOfURLs', disbish)
-       socket.on('firstcall', (data) => {
-            loopIPs()
-        })
-       
-    });
+io.on('connection', (socket) => {
+    io.emit('ListOfURLs', disbish)
+    socket.on('firstcall', (data) => {
+        loopIPs()
+    })
+});
+
+apis = (url, api, apiType) => {
+    console.log(url, api)
+    axios({
+        method: 'post',
+        url: `http://${url}/${apiType}`,
+        data: {  
+            "jsonrpc":"2.0",
+            "id":0,
+            "method":`${api}`
+         }
+    }).then((response) => {
+        console.log(response.data)
+    }).catch((response) => {
+        console.log(response)
+    })
+    // io.emit('configAPIObject', configObj)
+}
 
 global.holderObj = {}
 HeightsApi = (url) => {
@@ -118,6 +137,15 @@ loopIPs = () => {
         NetworkInfoApi(disbish[i])
         ConfigApi(disbish[i])
     }
+    for (let j = 0; j <= apisList.length-1; j++) {
+        for (let i = 0; i <= disbish.length-1; i++) {
+            if (v2List.includes(apisList[j])) {
+                apis(disbish[i], apisList[j], "v2")
+            } else {
+                apis(disbish[i], apisList[j], "debug")
+            } 
+        }
+    }  
     setInterval(() => {
         for (let i = 0; i <= disbish.length-1; i++) {
             HeightsApi(disbish[i])
