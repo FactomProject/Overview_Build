de1c2bf3820c24d6c5bb9cc3dacfe52e2c13d09a
Joshua Brigati <brigati.joshua@gmail.com>
2019-05-21 13:23:38 -0500

    WIP

--- src/App.jsx
+++ src/App.jsx
@@ -29,21 +29,36 @@ class App extends React.Component {
         "faulttimeout--current-minute",
         "roundtimeout--current-minute"
       ],
+      colorMode: "light"
     };
   }
 
+  ToggleColorMode = () => {
+    console.log("called")
+    this.setState({
+      colorMode: this.state.colorMode === "light" ? "dark" : "light"
+    })
+  }
+
   Main = () => {
+    let { colorMode, displayed, colVals } = this.state;
+    console.log(colorMode)
     return (
-      <div className="App">
+      <div className={`App ${colorMode}`}>
         <div>
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
             <h1 className="App-title">Information Display</h1>
+            <div className="mode-toggle" onClick={() => this.ToggleColorMode()}>
+            <div className="toggle">
+                <div id={colorMode === "dark" ? "dark-mode" : "light-mode"} type="checkbox"></div>
+            </div>
+        </div>
           </header>
           <div className="row">
             <Table
-              rowList={this.state.colVals}
-              displayed={this.state.displayed}
+              rowList={colVals}
+              displayed={displayed}
             />
           </div>
         </div>
